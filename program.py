0f2831acaf777507f0868c2ab3b9e0a5a3fa9c96
krushikar543 <77056197+krushikar543@users.noreply.github.com>
2022-11-25 10:39:32 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-def sum_of_n_numbers(n):
-    if n == 0:
-        return 0
-    return n + sum_of_n_numbers(n-1)
+def finonacci(n):
+    if n <= 1:
+        return n
+    return (fibonacci(n-1) + fibonacci(n-2))
