63cc54b035dcee2f61a11410d826b24cd2a33bdb
krushikar543 <77056197+krushikar543@users.noreply.github.com>
2022-11-25 10:45:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-from program import Fibonacci
+from program import finonacci
 import unittest
 
 class Test_Program(unittest.TestCase):
     def test_case_1(self):
-        r1 = Fibonacci(3)
+        r1 = finonacci(3)
         self.assertEqual(r1, 2)
     def test_case_2(self):
-        r2 = Fibonacci(5)
+        r2 = finonacci(5)
         self.assertEqual(r2,5)
     def test_case_3(self):
-        r3 = Fibonacci(7)
+        r3 = finonacci(7)
         self.assertEqual(r3,12)
     def test_case_4(self):
-        r4 = Fibonacci(10)
+        r4 = finonacci(10)
         self.assertEqual(r4,55)
     def test_case_5(self):
-        r5 = Fibonacci(12)
+        r5 = finonacci(12)
         self.assertEqual(r5,145)
 if __name__ == '__main__':
     unittest.main()
